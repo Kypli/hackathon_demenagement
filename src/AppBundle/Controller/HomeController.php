cf5cf8f73ebf48218281f87f2843248987e566bf
Pierre AMBOISE <pierre.amboise@yahoo.fr>
2017-12-13 17:34:03 +0100

    Estate

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -2,20 +2,25 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\EstateType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class HomeController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        // Display form
+        $form = $this->createForm(EstateType::class);
+
+        return $this->render('default/index.html.twig',
+            array(
+                'form' => $form->createView(),
+            )
+        );
     }
 }
---
+++ src/AppBundle/Form/EstateType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class EstateType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('estate', ChoiceType::class, array(
+            'choices' => array(
+                'Choisir une option' => '',
+                'Appartement' => array(
+                    'Studio' => '',
+                    '2 pièces' => '',
+                    '3 pièces' => '',
+                    '4 pièces' => '',
+                ),
+                'Maison' => array(
+                    '3 pièces' => '',
+                    '4 pièces' => '',
+                    '5 pièces' => '',
+                ),
+                'Inventaire libre' => array(
+                    'A faire soi-même' => '',
+                ),
+            ),
+        ));
+    }
+//
+//    /**
+//     * {@inheritdoc}
+//     */
+//    public function configureOptions(OptionsResolver $resolver)
+//    {
+//        $resolver->setDefaults(
+//            array(
+//                'data_class' => 'AppBundle\Entity\Specimen'
+//            )
+//        );
+//    }
+//
+//    /**
+//     * {@inheritdoc}
+//     */
+//    public function getBlockPrefix()
+//    {
+//        return 'appbundle_specimen';
+//    }
+}
