c2bc167bdfa06b7699ef416496d8cc8e52dfe29e
Pierre AMBOISE <pierre.amboise@yahoo.fr>
2017-12-13 21:28:48 +0100

    conflit

--- web/js/main.js
+++ web/js/main.js
@@ -1,14 +1,15 @@
 function copierColler(objet) {
 
     if (!document.getElementById(objet)) {
+
+        // Désactiver l'objet
+        document.getElementById('tag_' + objet).className = 'col-xs-2 tag bgGris';
+
         // Div 1
         maDiv = document.createElement("div");
         maDiv.id = objet;
         maDiv.className = 'col-xs-2 tag2';
         maDiv.innerHTML = objet;
-        maDiv.onclick = function(){
-            // this.remove();
-        };
         document.getElementById('inventory').appendChild(maDiv);
 
         // Div 3
@@ -18,13 +19,20 @@ function copierColler(objet) {
         maDiv.innerHTML = '<input id="' + objet + 'Number" type="number" class="number" value="1">';
         maDiv.onclick = function(){
            if (document.getElementById(objet + 'Number').value <= 0) {
+               // Supprimer div et div enfant des objets inventaire
                this.remove();
                document.getElementById(objet).remove();
+
+               // Réactiver objet
+               document.getElementById('tag_' + objet).className = 'col-xs-2 tag';
            };
         };
         document.getElementById(objet).append(maDiv);
     }
+}
 
+function afficherInventaire() {
+    if (){
 
-
+    }
 }
