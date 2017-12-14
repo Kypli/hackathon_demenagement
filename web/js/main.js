d32dc8bf2266c66177323432c80f0a1e91ac3ac3
Pierre AMBOISE <pierre.amboise@yahoo.fr>
2017-12-14 11:20:57 +0100

    Select

--- web/js/main.js
+++ web/js/main.js
@@ -2,28 +2,29 @@ function copierColler(objet) {
 
     if (!document.getElementById(objet)) {
 
-        // Désactiver l'objet
+        // Désactiver l'objet (gris)
         document.getElementById('tag_' + objet).className = 'col-xs-2 tag bgGris';
 
-        // Div 1
+        // Tag 1 (col G)
         maDiv = document.createElement("div");
         maDiv.id = objet;
         maDiv.className = 'col-xs-2 tag2';
         maDiv.innerHTML = objet;
         document.getElementById('inventory').appendChild(maDiv);
 
-        // Div 3
+        // Tag 3 (col D)
         maDiv = document.createElement("div");
         maDiv.id = objet + '2';
         maDiv.className = 'col-xs-2 tag3';
         maDiv.innerHTML = '<input id="' + objet + 'Number" type="number" class="number" value="1">';
         maDiv.onclick = function(){
            if (document.getElementById(objet + 'Number').value <= 0) {
-               // Supprimer div et div enfant des objets inventaire
+
+               // Supprimer Tag 2 et 3
                this.remove();
                document.getElementById(objet).remove();
 
-               // Réactiver objet
+               // Réactiver Tag 1 (vert)
                document.getElementById('tag_' + objet).className = 'col-xs-2 tag';
            };
         };
@@ -34,16 +35,16 @@ function copierColler(objet) {
 $( document ).ready(function() {
     $("select").on('click', function (e) {
 
+        var selectElmt = document.getElementById("estate_estate");
+        var valeurselectionnee = selectElmt.options[selectElmt.selectedIndex].value;
 
-        select = document.getElementsByClassName("selector");
-        choice = select.selectedIndex  // Récupération de l'index du <option> choisi
-        valeur_cherchee = select.options[choice].value;
-        alert(valeur_cherchee);
-
-        // if (valeur_cherchee == '3'){
-        //     document.getElementsByClassName('displayNone').style.display = 'block';
-        //     document.getElementById('tag_5').innerHTML = 'coucou'
-        // }
+        if (valeurselectionnee == '8'){
+            document.getElementById('colonneG').style.visibility="visible";
+            document.getElementById('colonneD').style.visibility="visible";
+        } else {
+            document.getElementById('colonneG').style.visibility="hidden";
+            document.getElementById('colonneD').style.visibility="hidden";
+        }
 
     })
 });
