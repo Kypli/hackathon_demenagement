1573c6dfddd0284238f597c1c73906cf22c0eada
christophe_p <chrispichon38@gmail.com>
2017-12-14 20:29:49 +0100

    fixtures sans les couleurs

--- src/AppBundle/DataFixtures/RoomFixtures.php
+++ src/AppBundle/DataFixtures/RoomFixtures.php
@@ -42,8 +42,8 @@ class RoomFixtures extends Fixture
         }
 
         // ajout de types
-        $typeFurnitures = [
-            "Cartons",
+        $typeFurnitures =
+            ["Cartons",
             "Canapé",
             "Siège",
             "Tables",
@@ -56,10 +56,9 @@ class RoomFixtures extends Fixture
             "Loisirs",
             "Rangements",
             "Literie",
-            "Autres",
-        ];
+            "Autres",];
 
-        foreach ($typeFurnitures as $key=>$name) {
+        foreach ($typeFurnitures as $key => $name) {
             $typeFurniture = new TypeFurniture();
             $typeFurniture->setName($name);
             $manager->persist($typeFurniture);
@@ -68,25 +67,25 @@ class RoomFixtures extends Fixture
 
         // ajout de pieces
         $pieceOfFurnitureCategories = [
-             ["Grand carton (55 x 35 x 30 cm)", "Petit carton (35 x 27,5 x 30 cm)"],
-             ["Canapé 2 places", "Canapé 3 places", "Canapé d'angle"],
-             ["Chaise", "Pouf", "Tabouret", "Fauteuil"],
-             ["Guéridon / Table d'appoint", "Table 2-4 personnes", "Table 6-8 personnes", "Table monastère en chêne", "Table Pliante", "Table Ronde"],
-             ["Halogène", "Lampe à abat-jour", "Lustre"],
-             ["Aquarium", "Miroir", "Tableau / Cadre", "Tapis"],
-             ["Armoire 1 porte", "Armoire 2 Portes", "Armoire 3 Portes", "Armoire 4 portes / dressing", "Bar", "Bibliothèque", "Bibliothèque Lourde", "Buffet 2 corps", "Buffet bas"],
-             ["Chaine Hi-Fi / Home cinema", "Enceinte HI/FI", "Ordinateur", "Petite imprimante", "Photocopieur bureau", "TV"],
-             ["Aspirateur", "Cave à vin", "Climatiseur", "Congélateur", "Cuisinière", "Four", "Four micro-onde", "Frigo Américain", "Hotte"],
-             ["Barbecue", "Brouette", "Caisse à outils", "Chaise longue", "Chaise longue", "Chaise PVC/extérieur"],
-             ["Arbre à chats", "Babyfoot", "Balancelle", "Banc/appareil musculation", "Billard", "Canne à pêche", "Flipper", "Guitare"],
-             ["Bac de rangement (50 x 60 x 40 cm maximum)", "Coffre / Malle / Cantine", "Pack 10 bouteilles", "Penderie 50cm linéaire", "Poubelle"],
-             ["Chauffeuse", "Lit 1 place", "Lit 2 places", "Lit bébé", "Lit Mezzanine"],
-             ["Rat empaillé"],
+            ["Grand carton (55 x 35 x 30 cm)", "Petit carton (35 x 27,5 x 30 cm)"],
+            ["Canapé 2 places", "Canapé 3 places", "Canapé d'angle"],
+            ["Chaise", "Pouf", "Tabouret", "Fauteuil"],
+            ["Guéridon / Table d'appoint", "Table 2-4 personnes", "Table 6-8 personnes", "Table monastère en chêne", "Table Pliante", "Table Ronde"],
+            ["Halogène", "Lampe à abat-jour", "Lustre"],
+            ["Aquarium", "Miroir", "Tableau / Cadre", "Tapis"],
+            ["Armoire 1 porte", "Armoire 2 Portes", "Armoire 3 Portes", "Armoire 4 portes / dressing", "Bar", "Bibliothèque", "Bibliothèque Lourde", "Buffet 2 corps", "Buffet bas"],
+            ["Chaine Hi-Fi / Home cinema", "Enceinte HI/FI", "Ordinateur", "Petite imprimante", "Photocopieur bureau", "TV"],
+            ["Aspirateur", "Cave à vin", "Climatiseur", "Congélateur", "Cuisinière", "Four", "Four micro-onde", "Frigo Américain", "Hotte"],
+            ["Barbecue", "Brouette", "Caisse à outils", "Chaise longue", "Chaise longue", "Chaise PVC/extérieur"],
+            ["Arbre à chats", "Babyfoot", "Balancelle", "Banc/appareil musculation", "Billard", "Canne à pêche", "Flipper", "Guitare"],
+            ["Bac de rangement (50 x 60 x 40 cm maximum)", "Coffre / Malle / Cantine", "Pack 10 bouteilles", "Penderie 50cm linéaire", "Poubelle"],
+            ["Chauffeuse", "Lit 1 place", "Lit 2 places", "Lit bébé", "Lit Mezzanine"],
+            ["Rat empaillé"],
         ];
 
 
-        foreach ($pieceOfFurnitureCategories as $key=>$pieceOfFurnitureCategory) {
-            foreach($pieceOfFurnitureCategory as $furniture) {
+        foreach ($pieceOfFurnitureCategories as $key => $pieceOfFurnitureCategory) {
+            foreach ($pieceOfFurnitureCategory as $furniture) {
                 $pieceOfFurniture = new PieceOfFurniture();
                 $pieceOfFurniture->setName($furniture);
                 $pieceOfFurniture->setTypeFurniture($types[$key]);
